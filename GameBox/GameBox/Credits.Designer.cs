90f2689251fd5a63d4bff49060d209175c701ec4
leorrose <leor.rose@gmail.com>
2018-11-25 13:15:37 +0200

    hadar

--- GameBox/GameBox/Credits.Designer.cs
+++ GameBox/GameBox/Credits.Designer.cs
@@ -28,6 +28,7 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Credits));
             this.Bt_ok = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
@@ -46,7 +47,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.BackgroundImage = global::GameBox.Properties.Resources.copyright_black_metal_sign_background_copyspace_32441277;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
             this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
             this.ClientSize = new System.Drawing.Size(998, 562);
             this.ControlBox = false;
