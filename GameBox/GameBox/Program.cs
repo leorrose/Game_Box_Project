0012471bcda057a8b58c0bf244a7b4d95f005bce
oritabibi <oritabibi7778@gmail.com>
2018-12-08 12:31:02 +0200

    shffle fix

--- GameBox/GameBox/Program.cs
+++ GameBox/GameBox/Program.cs
@@ -25,7 +25,7 @@ namespace GameBox
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Snake_and_ladders());
+            Application.Run(new MainForm());
 
         }
         public static string user1 = "", user2 = "";
